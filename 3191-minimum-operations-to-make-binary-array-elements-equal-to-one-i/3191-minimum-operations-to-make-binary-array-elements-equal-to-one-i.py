e1b2be9373dce0a1ff896b0b2bbb6568591aa0e0
mindahun <129931287+mindahun21@users.noreply.github.com>
2024-11-01 19:18:50 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -1,10 +1,20 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
         res = 0
-        for i in range(len(nums)-2):
-            if nums[i] ==0:
-                res +=1
-                for j in range(3):
-                    nums[i+j] = (nums[i+j] + 1) % 2
+        dq = deque([i for i, x in enumerate(nums) if x == 0])
+        
+        while dq:
+            i = dq.popleft()
+
+            if i > len(nums) -3 or nums[i] !=0:
+                continue
+            
+            for j in range(3):
+                nums[i+j] = (nums[i+j] + 1) % 2
+
+                if nums[i+j] ==0:
+                    dq.append(i+j)
+            
+            res+=1
         
         return -1 if 0 in nums else res
